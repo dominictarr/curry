03b9fba1b4454e10c1ef3ac1407992acd8a44b63
hugh fd jackson <hughfdjackson@googlemail.com>
2013-07-08 00:30:20 +0100

    Added eval hackry for ie

--- curry.js
+++ curry.js
@@ -1,6 +1,9 @@
 var slice = Array.prototype.slice;
 var toArray = function(a){ return slice.call(a) }
 
+//-- fn, [value] -> fn
+//-- create a curried function, incorporating any number of
+//-- pre-existing arguments (e.g. if you're further currying a function).
 var createFn = function(fn, args){
     var arity = fn.length - args.length;
 
@@ -18,14 +21,22 @@ var createFn = function(fn, args){
     return createEvalFn(fn, args, arity);
 }
 
-var createEvalFn = function(fn, args, arity){
-    var argList = makeArgList(arity);
-    var fnStr = '(function curriedFn(' + argList + '){ return processInvocation(fn, argify(args, arguments)); })';
-    return eval(fnStr);
-}
-
+//-- [value], arguments -> [value]
+// concat new arguments onto old arguments array
 var argify = function(args1, args2){
     return args1.concat(toArray(args2));
+}
+
+// fn, [value], int -> fn
+//-- create a function of the correct arity by the use of eval,
+//-- so that curry can handle functions of any arity
+var createEvalFn = function(fn, args, arity){
+    var argList = makeArgList(arity);
+
+    //-- hack for IE's faulty eval parsing -- http://stackoverflow.com/a/6807726
+    var fnStr = 'false||' +
+                'function curriedFn(' + argList + '){ return processInvocation(fn, argify(args, arguments)); }';
+    return eval(fnStr);
 }
 
 var makeArgList = function(len){
@@ -34,12 +45,18 @@ var makeArgList = function(len){
     return a.join(',');
 }
 
+//-- fn, [value] -> value
+//-- handle a function being invoked.
+//-- if the arg list is long enough, the function will be called
+//-- otherwise, a new curried version is created.
 var processInvocation = function(fn, args){
     if ( args.length > fn.length ) return fn.apply(null, args.slice(0, fn.length));
     if ( args.length === fn.length ) return fn.apply(null, args);
     return createFn(fn, args);
 }
 
+//-- fn -> fn
+//-- curries a function! <3
 var curry = function(fn){
     return createFn(fn, []);
 };
